445bbcc669c57e8594308c05f1cec002e89df66f
ngocquang2509-pro <111073078+ngocquang2509-pro@users.noreply.github.com>
2025-09-27 22:35:12 +0700

    first commit

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Chào em, anh có điều muốn nói. </br> Làm người yêu anh nhé!",
+  descriptionHeader:
+    "Hãy trả lời một cách thật lòng nhé. Nếu em im lặng và rời đi thì tức là em đã ĐỒNG Ý.",
+  buttonYes: "em đồng ý",
+  buttonNo: "Không đồng ý",
+  titleModar: "Cảm ơn em.",
+  descriptionModar:
+    "Cảm ơn em đã cho anh một cơ hội để anh có thể ở bên cạnh, che chở và bảo vệ em. </br> Anh biết anh không được đẹp trai, cũng chẳng lắm tài nhưng anh có một trái tim chân thành. Em chính là cả thế giới của anh. </br> Tối nay anh sẽ đến đón em. Em mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho em.</br> Love you <3",
+};
